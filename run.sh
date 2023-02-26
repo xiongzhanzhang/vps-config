12755430878463c4fb714b77ee0e331440d9e8c7
xiongzhanzhang <happybear2023@163.com>
2023-02-26 11:18:29 +0800

    fix

--- run.sh
+++ run.sh
@@ -3,9 +3,9 @@
 you_domian="xiongzhanzhang.xyz"
 you_passwd="password_placeholder"
 
-sed -i 's/xiongzhanzhang.xyz/${you_domian}/g' ./caddy.json
-sed -i 's/xiongzhanzhang.xyz/${you_domian}/g' ./nps.conf
-sed -i 's/password_placeholder/${you_passwd}/g' ./nps.conf
+sed -i "s/xiongzhanzhang.xyz/$you_domian/g" ./caddy.json
+sed -i "s/xiongzhanzhang.xyz/$you_domian/g" ./nps.conf
+sed -i "s/password_placeholder/$you_passwd/g" ./nps.conf
 cp nps.conf ./conf/nps.conf
 
 mkdir -p ~/share
